544508971df612132f878de22b6f9aa7e1998d74
fatihkaan1322-eng <fatihkaan1322@gmail.com>
2026-05-23 11:03:36 +0300

    cemberin alanı hesaplandı

--- index.js
+++ index.js
@@ -65,8 +65,9 @@ function CemberinCevresi(yariCap) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yariCap) {
+  const cemberinAlani = yariCap * yariCap * pi;
+  return cemberinAlani;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
